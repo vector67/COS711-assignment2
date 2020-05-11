6f96d2f7dc81885f6541b4d3a02356a359a934d3
Jeffrey Russell <jeffreymrussell@gmail.com>
2020-05-11 23:53:18 +0200

    Final push

--- code/src/data_manipulation/main.py
+++ code/src/data_manipulation/main.py
@@ -1,36 +1,83 @@
 import csv
 import matplotlib.pyplot as plt
-
-labels = ['relu', 'tanh', 'sigmoid', 'linear']
-legend = []
-for label in labels:
+import numpy as np
+bss = [8, 16, 32, 64, 128, 256, 512, 1024]
+legend_loss = []
+legend_accuracy = []
+val_times = []
+for bs in bss:
     val_loss_arr = []
     val_categorical_accuracy_arr = []
-    with open('../../output/loss_curves_' + label + '_hidden.csv', 'r') as f:
+    with open('../../output/batch_size_' + str(bs) + '_loss_curves.csv', 'r') as f:
         data = list(csv.reader(f))
         counter = 0
+        val_times_total = 0
         for row in data:
-            val_loss_sum = 0
-            val_categorical_accuracy_sum = 0
-            if not counter == 0:
-                for i in range(int(len(row)/2)):
-                    val_loss_sum += float(row[i*2])
-                    val_categorical_accuracy_sum += float(row[i*2 + 1])
-                total = len(row)/2
-                val_loss_arr.append(val_loss_sum / total)
-                val_categorical_accuracy_arr.append(val_categorical_accuracy_sum / total)
+            val_losses = list(map(float, row[0].split("|")))
+            val_categorical_accuracies = list(map(float, row[1].split("|")))
+            val_times_total += float(row[2])
+            for i in range(len(val_losses)):
+                if len(val_loss_arr) <= i:
+                    val_loss_arr.extend([[] for k in range(i - len(val_loss_arr)+1)])
+                    val_categorical_accuracy_arr.extend([[] for k in range(i - len(val_categorical_accuracy_arr)+1)])
+                # print(val_loss_arr, i)
+                val_loss_arr[i].append(val_losses[i])
+                val_categorical_accuracy_arr[i].append(val_categorical_accuracies[i])
             counter += 1
-        print(val_loss_arr)
-        print(val_categorical_accuracy_arr)
+        previous_len = 0
+        for i in range(len(val_loss_arr)):
+            if len(val_loss_arr[i]) < 8:
+                val_loss_arr = val_loss_arr[:i]
+                val_categorical_accuracy_arr = val_categorical_accuracy_arr[:i]
+                break
+            previous_len = len(val_loss_arr[i])
+            val_loss_arr[i] = sum(val_loss_arr[i]) / len(val_loss_arr[i])
+            val_categorical_accuracy_arr[i] = sum(val_categorical_accuracy_arr[i]) / \
+                                              len(val_categorical_accuracy_arr[i])
+        # print(val_loss_arr)
+        # print(val_categorical_accuracy_arr)
 
     # legend.append('loss_' + label + '(' + str(int(total)) + ')')
     # plt.plot(val_loss_arr)
-    legend.append('accuracy_' + label + '(' + str(int(total)) + ')')
+    legend_loss.append('loss_' + str(bs) + '(' + str(int(counter)) + ')')
+    legend_accuracy.append('accuracy_' + str(bs) + '(' + str(int(counter)) + ')')
+    plt.figure(2)
     plt.plot(val_categorical_accuracy_arr)
+    plt.figure(1)
+    plt.plot(val_loss_arr)
+    val_times.append(val_times_total / counter)
     counter += 1
+
+plt.figure(1)
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(legend_loss, loc='upper right')
+plt.savefig('../../output/batch_size_loss.png')
+
+plt.figure(2)
 plt.title('Model accuracy')
 plt.ylabel('Accuracy')
 plt.xlabel('Epoch')
-plt.legend(legend, loc='upper left')
+plt.legend(legend_accuracy, loc='lower right')
+plt.savefig('../../output/batch_size_accuracy.png')
+
+# plt.legend(legend_accuracy, loc='lower right')
 # plt.show()
-plt.savefig('../../output/accuracy.png')
+
+people = bss
+y_pos = np.arange(len(val_times))
+error = np.std(val_times)
+
+plt.figure(3)
+fig, ax = plt.subplots()
+ax.bar(y_pos, val_times, yerr=error, align='center')
+ax.set_xticks(y_pos)
+ax.set_xticklabels(people)
+# ax.invert_yaxis()  # labels read top-to-bottom
+ax.set_xlabel('Batch size')
+ax.set_ylabel('Time to run (s)')
+ax.set_title('Model run times in seconds')
+
+plt.tight_layout()
+plt.savefig('../../output/batch_size_times.png')
--- code/src/main.py
+++ code/src/main.py
@@ -13,7 +13,6 @@ from multiprocessing import Pool
 from src.constants import NUM_EPOCHS
 from src.pre_process import do_pre_process
 
-batch_size = 64
 # tf.random.set_seed(1234)
 plt.close('all')
 path_to_data = '../resources/winequality_data/'
@@ -83,7 +82,8 @@ def create_compiled_model(label):
     return model
 
 
-def do_training(label, curriculum_data, curriculum_labels, num_curriculum_buckets, min_epochs, layers=None):
+def do_training(label, curriculum_data, curriculum_labels, num_curriculum_buckets, min_epochs, layers=None,
+                batch_size=64):
     max_bucket_num = num_curriculum_buckets - 1
     print(layers)
     if layers is not None:
@@ -95,7 +95,7 @@ def do_training(label, curriculum_data, curriculum_labels, num_curriculum_bucket
     past_val_loss = []
     epoch_batch_number = 0
     my_loop_batch_size = 5
-
+    histories = []
     while True:
         epoch_number = epoch_batch_number * my_loop_batch_size
         # epoch_progress = epoch_number / min_epochs
@@ -107,9 +107,11 @@ def do_training(label, curriculum_data, curriculum_labels, num_curriculum_bucket
                             initial_epoch=epoch_number,
                             epochs=epoch_number + my_loop_batch_size, validation_data=(test_data, test_labels),
                             verbose=0)
-        print(len(current_curriculum_data), history.history['val_categorical_accuracy'][-1], history.history['val_loss'][-1])
+        print(len(current_curriculum_data), history.history['val_categorical_accuracy'][-1],
+              history.history['val_loss'][-1])
         if math.isnan(history.history['val_loss'][-1]):
             final_accuracy = 0
+            final_loss = 9999
             break
         past_val_loss.insert(0, history.history['val_loss'][-1])
         past_val_loss = past_val_loss[:20]
@@ -117,19 +119,22 @@ def do_training(label, curriculum_data, curriculum_labels, num_curriculum_bucket
         if len(past_val_loss) > comparison_range:
             num_increases = 0
             for k in range(1, comparison_range + 1):
-                if past_val_loss[k] - past_val_loss[k - 1] <= 0.01:
+                if past_val_loss[k] - past_val_loss[k - 1] <= 0.0001:
                     num_increases += 1
             if num_increases / comparison_range >= 0.6:
                 if epoch_number < min_epochs:
-                    print('would have broken, but we aren\'t because min epochs hasn\'t been reached')
+                    print('would have broken, but we aren\'t because min epochs hasn\'t been reached',
+                          min_epochs - epoch_number, 'left')
                 else:
                     print(past_val_loss)
                     print('ended with acc', history.history['val_categorical_accuracy'][-1])
                     final_accuracy = history.history['val_categorical_accuracy'][-1]
+                    final_loss = history.history['val_loss'][-1]
                     break
+        histories.append(history)
         epoch_batch_number += 1
     print('time', time.time() - start_time)
-    return final_accuracy
+    return final_loss, final_accuracy, histories
     # print(history.history)
     # loss_accuracy_curve = []
     # for epoch in range(NUM_EPOCHS):
@@ -163,124 +168,165 @@ def do_training(label, curriculum_data, curriculum_labels, num_curriculum_bucket
     # np.savetxt('../output/loss_curves_' + label + '_hidden.csv', data, delimiter=',', fmt='%s')
 
 
-def score_population(population_to_be_scored):
-    scores = [score for i, score in population_to_be_scored]
-    counter = 0
-    for individual in population_to_be_scored:
-        individual_genes = individual[0]
-        if individual[1] == 0:
-            # fitness calculation:
-            print(train_data.shape)
-            print(train_labels.shape)
-            # scores[counter] = do_training('tanh', train_data, train_labels, num_buckets, 200, layers=individual[0])
-            scores[counter] = random.randint(0, 100)
-        counter += 1
-    return scores
+bss = [8, 16, 32, 64, 128, 256, 512, 1024]
+# bss = [64]
+for i in range(0, 30):
+    for bs in bss:
+        start_time = time.time()
+        loss, accuracy, histories = do_training('tanh', train_data, train_labels, 5, 50, batch_size=bs)
+        total_time = time.time() - start_time
+        losses = []
+        accuracies = []
+        for history in histories:
+            # print(history.history)
+            losses.extend(history.history['val_loss'])
+            accuracies.extend(history.history['val_categorical_accuracy'])
 
+        # print(losses)
+        # print(accuracies)
+        filename = "../output/batch_size_" + str(bs) + "_loss_curves.csv"
+        if not os.path.exists(filename):
+            open(filename, 'w').close()
+        with open(filename, newline='\n') as csvfile:
+            reader_rows = csv.reader(csvfile, delimiter=',', quotechar='"')
+            rows = []
+            for row in reader_rows:
+                rows.append(row)
+        rows.append(["|".join(map(str, losses)), "|".join(map(str, accuracies)), total_time])
+        print(rows)
+        # writer = csv.writer(open(filename, "wb"))
+        # writer.writerows(np.array(loss_accuracy_curve))
+        np.savetxt(filename, rows, delimiter=',', fmt='%s')
 
-def sort_second(val):
-    return val[1]
-
-
-chance_for_name_mutation = 0.2
-chance_for_activation_function_mutation = 0.2
-chance_to_gain_layer = 0.02
-chance_to_lose_layer = 0.02
-
-
-def random_layer():
-    return Layer(random.choice(valid_layer_names),
-                 random.randint(0, 100),
-                 random.choice(valid_layer_activation_functions))
-
-
-def mutate_layers(individual):
-    for ind_layer in individual:
-        if random.random() < mutation_rate:
-            attribute_to_change = "num_neurons"
-            if random.random() < chance_for_name_mutation:
-                attribute_to_change = "name"
-            elif random.random() < chance_for_activation_function_mutation / chance_for_name_mutation:
-                attribute_to_change = "activation"
-            if attribute_to_change == "num_neurons":
-                ind_layer.number_of_neurons += int(math.pow(random.randint(0, 5), 1.5) - 5.6)
-            elif attribute_to_change == "name":
-                ind_layer.name = random.choice(valid_layer_names)
-            elif attribute_to_change == "activation":
-                ind_layer.activation_function = random.choice(valid_layer_activation_functions)
-    if random.random() < chance_to_gain_layer:
-        individual.append(random_layer())
-    if random.random() < chance_to_lose_layer:
-        selection = random.randint(0, len(individual)-1)
-        individual.pop(selection)
-
-
-def crossover_layers(individual1, individual2):
-    new_individual1 = individual1[: int(len(individual1) / 2)] + individual2[int(len(individual2) / 2):]
-    new_individual2 = individual2[: int(len(individual2) / 2)] + individual1[int(len(individual1) / 2):]
-    return new_individual1, new_individual2
-
-
-# do_training('tanh')
-num_training_patterns = len(train_data)
-num_buckets = 5
-
-curriculum = np.arange(num_training_patterns)
-
-population_size = 4
-population = []
-num_generations = 1
-mutation_rate = 0.1
-mutation_vs_crossover_rate = 0.5
-
-num_threads = 4
-
-filename = '../output/best_architecture.csv'
-if os.path.exists(filename):
-    with open(filename, 'r') as f:
-        for line in f:
-            line_arr = line.split(',')
-            population.append([list(map(int, line_arr[0].split("|"))), float(line_arr[1])])
-        print(population)
-
-for i in range(population_size - len(population)):
-    genes = []
-    for k in range(random.randint(0,10)):
-        genes.append(random_layer())
-    population.append([genes, 0])
-
-if __name__ == '__main__':
-    for generation_number in range(num_generations):
-        print('\n\n\nStarting generation:', generation_number)
-        population = population[:population_size]
-        print(population)
-        with Pool(num_threads) as p:
-            new_scores = np.array(p.map(score_population, np.split(np.array(population), num_threads))).flatten()
-        population = [[ind[0], score] for [ind, score] in zip(population, new_scores)]
-        population.sort(key=sort_second, reverse=True)
-        population = population[:int(population_size / 2)]
-        for k in range(population_size - len(population)):
-            if random.random() < mutation_vs_crossover_rate:
-                newbie_genes1, newbie_genes2 = crossover_layers(population[np.random.choice(len(population))][0],
-                                                                population[np.random.choice(len(population))][0])
-                population.append([newbie_genes1, 0])
-                k += 1
-                if k < population_size - len(population):
-                    population.append([newbie_genes2, 0])
-            else:
-                parent = population[np.random.choice(len(population))]
-                newbie_genes = []
-                for layer in parent[0]:
-                    newbie_genes.append(layer.copy())
-                mutate_layers(newbie_genes)
-                # population.append([newbie_genes, parent[1] + random.randint(-10, 10)])
-                population.append([newbie_genes, 0])
-    print(population)
-print(population[0])
-final_array = []
-for pop in population:
-    final_array.append(["|".join(map(str, pop[0])), pop[1]])
-np.savetxt('../output/best_layers.csv', final_array, delimiter=',', fmt='%s')
+# print('evaluating: ')
+# print(model.evaluate(test_data, test_labels, verbose=2))
+# print(test_data[0], test_labels[0])
+# # for label in test_labels:
+# #     print(label)
+# for i in range(len(test_labels)):
+#     print('correct answer was', np.argmax(test_labels[i]), 'model predicted:',
+#           np.argmax(model.predict([[test_data[i]]])))
+# print(model.layers[0].get_weights()[0])
+# print(model.layers[1].get_weights()[0])
+#
+# def score_population(population_to_be_scored):
+#     scores = [score for i, score in population_to_be_scored]
+#     counter = 0
+#     for individual in population_to_be_scored:
+#         individual_genes = individual[0]
+#         if individual[1] == 0:
+#             # fitness calculation:
+#             print(train_data.shape)
+#             print(train_labels.shape)
+#             # scores[counter] = do_training('tanh', train_data, train_labels, num_buckets, 200, layers=individual[0])
+#             scores[counter] = random.randint(0, 100)
+#         counter += 1
+#     return scores
+#
+#
+# def sort_second(val):
+#     return val[1]
+#
+#
+# chance_for_name_mutation = 0.2
+# chance_for_activation_function_mutation = 0.2
+# chance_to_gain_layer = 0.02
+# chance_to_lose_layer = 0.02
+#
+#
+# def random_layer():
+#     return Layer(random.choice(valid_layer_names),
+#                  random.randint(0, 100),
+#                  random.choice(valid_layer_activation_functions))
+#
+#
+# def mutate_layers(individual):
+#     for ind_layer in individual:
+#         if random.random() < mutation_rate:
+#             attribute_to_change = "num_neurons"
+#             if random.random() < chance_for_name_mutation:
+#                 attribute_to_change = "name"
+#             elif random.random() < chance_for_activation_function_mutation / chance_for_name_mutation:
+#                 attribute_to_change = "activation"
+#             if attribute_to_change == "num_neurons":
+#                 ind_layer.number_of_neurons += int(math.pow(random.randint(0, 5), 1.5) - 5.6)
+#             elif attribute_to_change == "name":
+#                 ind_layer.name = random.choice(valid_layer_names)
+#             elif attribute_to_change == "activation":
+#                 ind_layer.activation_function = random.choice(valid_layer_activation_functions)
+#     if random.random() < chance_to_gain_layer:
+#         individual.append(random_layer())
+#     if random.random() < chance_to_lose_layer:
+#         selection = random.randint(0, len(individual)-1)
+#         individual.pop(selection)
+#
+#
+# def crossover_layers(individual1, individual2):
+#     new_individual1 = individual1[: int(len(individual1) / 2)] + individual2[int(len(individual2) / 2):]
+#     new_individual2 = individual2[: int(len(individual2) / 2)] + individual1[int(len(individual1) / 2):]
+#     return new_individual1, new_individual2
+#
+#
+# # do_training('tanh')
+# num_training_patterns = len(train_data)
+# num_buckets = 5
+#
+# curriculum = np.arange(num_training_patterns)
+#
+# population_size = 4
+# population = []
+# num_generations = 1
+# mutation_rate = 0.1
+# mutation_vs_crossover_rate = 0.5
+#
+# num_threads = 4
+#
+# filename = '../output/best_architecture.csv'
+# if os.path.exists(filename):
+#     with open(filename, 'r') as f:
+#         for line in f:
+#             line_arr = line.split(',')
+#             population.append([list(map(int, line_arr[0].split("|"))), float(line_arr[1])])
+#         print(population)
+#
+# for i in range(population_size - len(population)):
+#     genes = []
+#     for k in range(random.randint(0,10)):
+#         genes.append(random_layer())
+#     population.append([genes, 0])
+#
+# if __name__ == '__main__':
+#     for generation_number in range(num_generations):
+#         print('\n\n\nStarting generation:', generation_number)
+#         population = population[:population_size]
+#         print(population)
+#         with Pool(num_threads) as p:
+#             new_scores = np.array(p.map(score_population, np.split(np.array(population), num_threads))).flatten()
+#         population = [[ind[0], score] for [ind, score] in zip(population, new_scores)]
+#         population.sort(key=sort_second, reverse=True)
+#         population = population[:int(population_size / 2)]
+#         for k in range(population_size - len(population)):
+#             if random.random() < mutation_vs_crossover_rate:
+#                 newbie_genes1, newbie_genes2 = crossover_layers(population[np.random.choice(len(population))][0],
+#                                                                 population[np.random.choice(len(population))][0])
+#                 population.append([newbie_genes1, 0])
+#                 k += 1
+#                 if k < population_size - len(population):
+#                     population.append([newbie_genes2, 0])
+#             else:
+#                 parent = population[np.random.choice(len(population))]
+#                 newbie_genes = []
+#                 for layer in parent[0]:
+#                     newbie_genes.append(layer.copy())
+#                 mutate_layers(newbie_genes)
+#                 # population.append([newbie_genes, parent[1] + random.randint(-10, 10)])
+#                 population.append([newbie_genes, 0])
+#     print(population)
+# print(population[0])
+# final_array = []
+# for pop in population:
+#     final_array.append(["|".join(map(str, pop[0])), pop[1]])
+# np.savetxt('../output/best_layers.csv', final_array, delimiter=',', fmt='%s')
 
 
 # num_training_patterns = len(train_data)
@@ -329,17 +375,3 @@ np.savetxt('../output/best_layers.csv', final_array, delimiter=',', fmt='%s')
 # for pop in population:
 #     final_array.append(["|".join(map(str, pop[0])), pop[1]])
 # np.savetxt('../output/best_order.csv', final_array, delimiter=',', fmt='%s')
-
-
-# writer = csv.writer(open("../output/loss_curves_relu_hidden.csv", "wb"))
-# writer.writerows(np.array(loss_accuracy_curve))
-# print('evaluating: ')
-# print(model.evaluate(test_data, test_labels, verbose=2))
-# print(test_data[0], test_labels[0])
-# # for label in test_labels:
-# #     print(label)
-# for i in range(len(test_labels)):
-#     print('correct answer was', np.argmax(test_labels[i]), 'model predicted:',
-#           np.argmax(model.predict([[test_data[i]]])))
-# print(model.layers[0].get_weights()[0])
-# print(model.layers[1].get_weights()[0])
